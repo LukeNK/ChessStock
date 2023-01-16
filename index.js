db7a618ca7991b38745179b5a07c654e8b4779bf
LukeNK <lukenguyenkhanh@gmail.com>
2023-01-15 20:33:52 -0800

    Re-decorate the graph

--- index.js
+++ index.js
@@ -9,29 +9,16 @@ https.get('https://lichess.org/api/games/user/LukeNK?tags=true&clocks=false&eval
 
         const user = 'LukeNK' // LukeNK
         let luke = undefined, // white 0, black 1
-            time = false, 
             elo = [];
 
         data = data.split('\n');
         data.forEach((line) => {
             if (line == `[White "${user}"]`) luke = 0; 
             else if (line == `[Black "${user}"]`) luke = 1;
-
-            // set time control, CAUTION: CHESS.COM IS DIFFERENT FROM LICHESS
-            if (line == '[TimeControl "600+0"]' || line == '[TimeControl "600"]') time = true;
-
-            if (
-                (luke == 0 && line.slice(0, 11) == '[WhiteElo "') ||
-                (luke == 1 && line.slice(0, 11) == '[BlackElo "')
-            ) {
-                for (let l1 = 0; l1 < line.length; l1++)
-                    if (isNaN(parseInt(line[l1])))
-                        line = line.substring(0, l1) + " " + line.substring(l1 + 1, line.length);
-                line = line.replace(' ', '');
-                elo.push(parseInt(line));
-                // flag for next game
-                time = false;
-            }
+            else if (
+                (luke == 0 && line.includes('[WhiteRatingDiff "')) ||
+                (luke == 1 && line.includes('[BlackRatingDiff "'))
+            ) elo.push(parseInt(line.split('"')[1]));
         });
         fs.writeFileSync('out.js', 'data = ' + JSON.stringify(elo, null, '\t'), 'utf-8')
     })
